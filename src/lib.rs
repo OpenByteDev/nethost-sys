b155bf0f51171f5ace1487bb67a9fee084bd87e5
OpenByte <development.openbyte@gmail.com>
2022-06-28 20:23:19 +0200

    Revert "Remove linking weirdness fix"
    This reverts commit a9e255cdec7862e3d8f640da1646f7c3ba87aa53.

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,8 @@
 use core::{mem, ptr};
 use coreclr_hosting_shared::{char_t, size_t};
 
-#[cfg_attr(windows, link(name = "libnethost"))]
+// for some reason we need the link attribute here for unix, but the rustc argument in build.rs for windows.
+// #[cfg_attr(windows, link(name = "libnethost"))]
 #[cfg_attr(unix, link(name = "nethost", kind = "static"))]
 #[cfg_attr(
     all(unix, not(target_os = "macos")),
